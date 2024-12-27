0358fce9dc11e0900f9c5c289af72e733f947616
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2024-12-27 19:11:54 +0330

    config localization package

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class L10n {
+  static const Locale en = Locale('en');
+  static const Locale fa = Locale('fa');
+  static const Locale system = Locale('system');
+  static final List<Locale> all = <Locale>[en, fa];
+}
--- lib/super_tic_tac_toe_app.dart
+++ lib/super_tic_tac_toe_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:super_tic_tac_toe/infrastructure/router/router.dart';
+import 'package:super_tic_tac_toe/l10n/l10n.dart';
 
 class SuperTicTacToeApp extends StatelessWidget {
   const SuperTicTacToeApp({super.key});
@@ -7,6 +9,8 @@ class SuperTicTacToeApp extends StatelessWidget {
   // This widget is the root of application.
   @override
   Widget build(BuildContext context) {
+    Locale locale = L10n.en;
+
     return MaterialApp.router(
       routerConfig: AppRouter.router,
       title: 'Flutter Demo',
@@ -14,6 +18,13 @@ class SuperTicTacToeApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
         useMaterial3: true,
       ),
+      supportedLocales: L10n.all,
+      locale: locale != L10n.system ? locale : null,
+      localizationsDelegates: const [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
     );
   }
 }
