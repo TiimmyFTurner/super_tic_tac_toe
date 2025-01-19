21b3df6de8f10dc7c71c0041937e36fdf73df949
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2025-01-19 09:42:08 +0330

    config router, add 3x3 game mode pages

--- lib/infrastructure/router/router.dart
+++ lib/infrastructure/router/router.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:super_tic_tac_toe/infrastructure/router/router_consts.dart';
-import 'package:super_tic_tac_toe/presentation/screens/play_3x3_screen.dart';
+import 'package:super_tic_tac_toe/presentation/screens/main_menu.dart';
+import 'package:super_tic_tac_toe/presentation/screens/play_3x3_bot_screen.dart';
+import 'package:super_tic_tac_toe/presentation/screens/play_3x3_no_tie_bot_screen.dart';
+import 'package:super_tic_tac_toe/presentation/screens/play_3x3_no_tie_pvp_screen.dart';
+import 'package:super_tic_tac_toe/presentation/screens/play_3x3_pvp_screen.dart';
 
 class AppRouter {
   static final _rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -11,8 +15,23 @@ class AppRouter {
     routes: <RouteBase>[
       GoRoute(
         path: Routes.homeRoutePath,
-        builder: (context, state) => const Play3x3Screen(),
+        builder: (context, state) => const MainMenu(),
       ),
+      GoRoute(
+        path: Routes.play3x3PvP,
+        builder: (context, state) => const Play3x3PvPScreen(),
+      ),
+      GoRoute(
+        path: Routes.play3x3bot,
+        builder: (context, state) => const Play3x3BotScreen(),
+      ),
+      GoRoute(
+        path: Routes.play3x3NoTiePvP,
+        builder: (context, state) => const Play3x3NoTiePvPScreen(),
+      ),
+      GoRoute(
+          path: Routes.play3x3NoTieBot,
+          builder: (context, state) => const Play3x3NoTieBotScreen())
     ],
   );
 
--- lib/infrastructure/router/router_consts.dart
+++ lib/infrastructure/router/router_consts.dart
@@ -1,3 +1,7 @@
 class Routes {
   static const String homeRoutePath = '/home';
+  static const String play3x3PvP = '/play3x3PvP';
+  static const String play3x3bot = '/play3x3bot';
+  static const String play3x3NoTiePvP = '/play3x3NoTiePvP';
+  static const String play3x3NoTieBot = '/play3x3NoTieBot';
 }
