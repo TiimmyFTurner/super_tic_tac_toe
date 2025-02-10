c3bb9823d5d720d7a9ffa0bc8c092a73e221472c
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2025-02-10 18:18:18 +0330

    add koodak font for persian languag

--- lib/super_tic_tac_toe_app.dart
+++ lib/super_tic_tac_toe_app.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,7 +8,6 @@ import 'package:super_tic_tac_toe/infrastructure/router/router.dart';
 import 'package:super_tic_tac_toe/l10n/l10n.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-
 class SuperTicTacToeApp extends ConsumerWidget {
   const SuperTicTacToeApp({super.key});
 
@@ -14,22 +15,21 @@ class SuperTicTacToeApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     Locale locale = ref.watch(localeSettingProvider);
+    String? font = locale == L10n.fa ? 'Koodak' : null;
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: AppRouter.router,
       title: 'Super Tic-Tac-Toe',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
+          useMaterial3: true,
+          fontFamily: font),
       darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.tealAccent,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.tealAccent, brightness: Brightness.dark),
+          useMaterial3: true,
+          fontFamily: font),
       themeMode: ref.watch(themeModeSettingProvider),
       supportedLocales: L10n.all,
       locale: locale != L10n.system ? locale : null,
